d8141b35ac5fe2f3a665c7b7d4ababf5d2e23821
Sara Kowalski <sarakowalski08@gmail.com>
2020-10-18 14:56:50 -0400

    feat: removed links

--- src/pages/Elements.js
+++ src/pages/Elements.js
@@ -86,9 +86,6 @@ const IndexPage = () => <Layout>
 							</ol>
 							<h5>Icons</h5>
 							<ul className="icons">
-								<li><a href="/#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-								<li><a href="/#" className="icon brands fa-facebook-f"><span className="label">Facebook</span></a></li>
-								<li><a href="/#" className="icon brands fa-instagram"><span className="label">Instagram</span></a></li>
 								<li><a href="/#" className="icon brands fa-github"><span className="label">Github</span></a></li>
 							</ul>
 						</div>
