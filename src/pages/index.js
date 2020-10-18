a9999eead6d0a8496dfa8e3a7b7e93803a708c8e
Sara Kowalski <sarakowalski08@gmail.com>
2020-10-18 14:09:18 -0400

    feat: added plantasia project

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,23 +38,23 @@ const IndexPage = () => (
             A Little About Me
           </h2>
           <p>
-          I am a bio/pre-med student turn software engineer. In 2017, I graduated from Queens College with a bachelors in bio/pre-med. However, after working in healthcare, I realized I could put my talents to better use and I felt that tech was just the place for that. I have a diverse set of interests that make me a well rounded engineer, which includes  painting, make up, video games, fashion, and, of course, coding.
+          I am a bio/pre-med student turn software engineer. In 2017, I graduated from Queens College with a bachelors in bio/pre-med. However, after working in healthcare, I realized I could put my talents to better use and I felt that tech was just the place for that. As a result, I attended The Grace Hopper Program through Fullstack Academy to help jumpstart my career as a software engineer. I have a diverse set of interests that make me a well rounded engineer, which includes  painting, make up, video games, fashion, and, of course, coding.
           </p>
         </header>
         <ul className="icons major">
           <li>
-            <span className="icon fa-gem major style1">
-              <span className="label">Lorem</span>
-            </span>
-          </li>
-          <li>
             <span className="icon fa-heart major style2">
-              <span className="label">Ipsum</span>
+              <span className="label">Heart</span>
             </span>
           </li>
           <li>
             <span className="icon solid fa-code major style3">
-              <span className="label">Dolor</span>
+              <span className="label">Code</span>
+            </span>
+          </li>
+          <li>
+            <span className="icon fa-heart major style2">
+              <span className="label">Heart</span>
             </span>
           </li>
         </ul>
@@ -68,14 +68,15 @@ const IndexPage = () => (
         </div>
         <div className="content">
           <h2>
-            Magna primis lobortis
+            <a href="https://plantasia.herokuapp.com/<">Plantasia</a>
             <br />
-            sed ullamcorper
+            Full Stack Developer
           </h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
-            imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
-          </p>
+          <ul>
+            <li>Developed database schemas using Sequelize for easy data manipulations</li>
+            <li>Expanded the seed file with mock data for demonstration purposes</li>
+            <li>Created react-redux frontend components for user signup and login for seamless user addition to e-commerce database</li>
+          </ul>
         </div>
       </section>
       <section className="spotlight">
