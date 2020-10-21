3f6f70d25d9c72d341e05b765521d8d0b1a0f389
Sara Kowalski <sarakowalski08@gmail.com>
2020-10-20 20:52:39 -0400

    fix: added id to div for anchor links

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,26 +5,28 @@ import Scroll from '../components/Scroll';
 
 const Landing = () => {
   return (
-    <section id="banner">
-      <div className="inner">
-        <h2>{config.heading}</h2>
-        <p>{config.subHeading}</p>
-        <ul className="actions special">
-          <li>
-            <Scroll type="id" element="one">
-              <a href="/#" className="button primary">
-                Explore
-              </a>
-            </Scroll>
-          </li>
-        </ul>
-      </div>
-      <Scroll type="id" element="one">
-        <a href="#one" className="more">
-          Learn More
-        </a>
-      </Scroll>
-    </section>
+    <div id="landing">
+      <section id="banner">
+        <div className="inner">
+          <h2>{config.heading}</h2>
+          <p>{config.subHeading}</p>
+          <ul className="actions special">
+            <li>
+              <Scroll type="id" element="one">
+                <a href="/#" className="button primary">
+                  Explore
+                </a>
+              </Scroll>
+            </li>
+          </ul>
+        </div>
+        <Scroll type="id" element="one">
+          <a href="#one" className="more">
+            Learn More
+          </a>
+        </Scroll>
+      </section>
+    </div>
   )
 }
 
--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -2,89 +2,89 @@ import React from 'react';
 
 const WorkExp = () => {
   return (
-    <div>
-    <section id="three" className="wrapper style3 special">
-      <div className="inner">
-        <header className="major">
-          <h2>Accumsan mus tortor nunc aliquet</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
-            eleifend
-            <br />
-            fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
-            ullamcorper.
-          </p>
-        </header>
-        <ul className="features">
-          <li className="icon fa-paper-plane">
-            <h3>Arcu accumsan</h3>
+    <div id="work">
+      <section id="three" className="wrapper style3 special">
+        <div className="inner">
+          <header className="major">
+            <h2>Accumsan mus tortor nunc aliquet</h2>
             <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
+              Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
+              eleifend
+              <br />
+              fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
+              ullamcorper.
             </p>
-          </li>
-          <li className="icon solid fa-laptop">
-            <h3>Ac Augue Eget</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-code">
-            <h3>Mus Scelerisque</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon solid fa-headphones-alt">
-            <h3>Mauris Imperdiet</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-heart">
-            <h3>Aenean Primis</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-          <li className="icon fa-flag">
-            <h3>Tortor Ut</h3>
-            <p>
-              Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
-              tincidunt nullam amet leo Aenean ligula consequat consequat.
-            </p>
-          </li>
-        </ul>
-      </div>
-    </section>
+          </header>
+          <ul className="features">
+            <li className="icon fa-paper-plane">
+              <h3>Arcu accumsan</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+            <li className="icon solid fa-laptop">
+              <h3>Ac Augue Eget</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+            <li className="icon solid fa-code">
+              <h3>Mus Scelerisque</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+            <li className="icon solid fa-headphones-alt">
+              <h3>Mauris Imperdiet</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+            <li className="icon fa-heart">
+              <h3>Aenean Primis</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+            <li className="icon fa-flag">
+              <h3>Tortor Ut</h3>
+              <p>
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-    <section id="cta" className="wrapper style4">
-      <div className="inner">
-        <header>
-          <h2>Arcue ut vel commodo</h2>
-          <p>
-            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
-            eleifend fringilla.
-          </p>
-        </header>
-        <ul className="actions stacked">
-          <li>
-            <a href="/#" className="button fit primary">
-              Activate
-            </a>
-          </li>
-          <li>
-            <a href="/#" className="button fit">
-              Learn More
-            </a>
-          </li>
-        </ul>
-      </div>
-    </section>
+      <section id="cta" className="wrapper style4">
+        <div className="inner">
+          <header>
+            <h2>Arcue ut vel commodo</h2>
+            <p>
+              Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
+              eleifend fringilla.
+            </p>
+          </header>
+          <ul className="actions stacked">
+            <li>
+              <a href="/#" className="button fit primary">
+                Activate
+              </a>
+            </li>
+            <li>
+              <a href="/#" className="button fit">
+                Learn More
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   )
 }
