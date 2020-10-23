4d3cd02e3940c478c7fd7d5c8c4ca631456c8df1
Sara Kowalski <sarakowalski08@gmail.com>
2020-10-22 20:15:44 -0400

    feat: added dropdown feature

--- src/components/WorkExp.js
+++ src/components/WorkExp.js
@@ -9,53 +9,65 @@ const WorkExp = () => {
             <h2>My Work Experience</h2>
           </header>
           <ul className="features">
-            <li className="icon fa-paper-plane">
-              <h3>Vendor in Bloomingdale's</h3>
-              <p>
-                Sale's Representative
-                <br />
-                September 2016 - February 2020
-              </p>
-              <ul>
-                <li>
-                Assisted operations management including conference calls, processing shipment, reporting statistics including calculating the business for daily, weekly, monthly transactions such as units per transaction, average transaction value, styles that performed based on percent up or down from year to date, operational and clientele updates, and shop comparisons by year
-                </li>
-                <li> Engaged customers and contributed to the shop’s daily sales target of $2,000 by meeting a personal sales goal of $500-$700 per shift</li>
-              </ul>
-            </li>
-            <li className="icon solid fa-heart">
-              <h3>St. Francis Hospital</h3>
-              <p>
-                Patient Care Assistant
-                <br />
-                April 2018 - September 2019
-              </p>
-              <ul>
-                <li>Monitored telemetry rhythms daily for 6-13 patients daily to ensure that there were no unsafe changes in patients heart beat</li>
-                <li>
-                Responded immediately and appropriately during emergency situations, which includes EKGs, checking blood sugar, and even CPR and defibrillation if necessary
-                </li>
-              </ul>
-            </li>
-            <li className="icon solid fa-flag">
-              <h3>Harbor Freight Tools</h3>
-              <p>
-                Sales Associate
-                <br />
-                July 2014 - September 2016
-              </p>
-              <ul>
-                <li>
-                Trained approximately 5 new hires annually on operational procedures, including equipment use and maintenance, customer service, and adhering to safety compliance standards
-                </li>
-                <li>
-                Successfully maintained personal sales goal of over 90% sales in higher price-point warranties
-                </li>
-                <li>
-                Contributed to building the store, including building fixtures and filling product while learning operation procedures
-                </li>
-              </ul>
-            </li>
+            <div className="dropdown">
+              <li className="icon fa-paper-plane">
+                <h3>Vendor in Bloomingdale's</h3>
+                <p>
+                  Sale's Representative
+                  <br />
+                  September 2016 - February 2020
+                </p>
+                <div className="dropdown-content">
+                  <ul>
+                    <li>
+                    Assisted operations management including conference calls, processing shipment, reporting statistics including calculating the business for daily, weekly, monthly transactions such as units per transaction, average transaction value, styles that performed based on percent up or down from year to date, operational and clientele updates, and shop comparisons by year
+                    </li>
+                    <li> Engaged customers and contributed to the shop’s daily sales target of $2,000 by meeting a personal sales goal of $500-$700 per shift</li>
+                  </ul>
+                </div>
+              </li>
+            </div>
+            <div className="dropdown">
+              <li className="icon solid fa-heart">
+                <h3>St. Francis Hospital</h3>
+                <p>
+                  Patient Care Assistant
+                  <br />
+                  April 2018 - September 2019
+                </p>
+                <div className="dropdown-content">
+                  <ul>
+                    <li>Monitored telemetry rhythms daily for 6-13 patients daily to ensure that there were no unsafe changes in patients heart beat</li>
+                    <li>
+                    Responded immediately and appropriately during emergency situations, which includes EKGs, checking blood sugar, and even CPR and defibrillation if necessary
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </div>
+            <div className="dropdown">
+              <li className="icon solid fa-flag">
+                <h3>Harbor Freight Tools</h3>
+                <p>
+                  Sales Associate
+                  <br />
+                  July 2014 - September 2016
+                </p>
+                <div className="dropdown-content">
+                  <ul>
+                    <li>
+                    Trained approximately 5 new hires annually on operational procedures, including equipment use and maintenance, customer service, and adhering to safety compliance standards
+                    </li>
+                    <li>
+                    Successfully maintained personal sales goal of over 90% sales in higher price-point warranties
+                    </li>
+                    <li>
+                    Contributed to building the store, including building fixtures and filling product while learning operation procedures
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </div>
           </ul>
         </div>
       </section>
